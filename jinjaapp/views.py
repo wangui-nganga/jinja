b2c6cac8e5c773191819c293af33672bdbe87721
wangui-nganga <56192567+wangui-nganga@users.noreply.github.com>
2024-11-08 09:12:13 +0300

    first commit

---
+++ jinjaapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request,'home.html')
+def about(request):
+    return render(request,'about.html')
+def contact(request):
+    return render(request,'contact.html')
+def base(request):
+    return render(request,'base.html')
+def services(request):
+    return render(request,'services.html')
+
